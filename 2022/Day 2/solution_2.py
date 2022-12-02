2206f59f3b016eba721129182fc2871825a91084
Cameron Ford <64077043+JuggleHacker@users.noreply.github.com>
2022-12-02 11:22:14 +0000

    Improve solution 2

--- 2022/Day 2/solution_2.py
+++ 2022/Day 2/solution_2.py
@@ -1,16 +1,12 @@
-def chose(A, B):
-    if B == 'Y':
-        return ['rock', 'paper', 'scissors']['ABC'.index(A)]
-    beats = {"A" : "paper", "B": "scissors", "C": "rock"}
-    if B == 'Z':
-        return beats[A]
-    loses = {'A': 'scissors', 'B': 'rock', 'C': 'paper'}
-    if B == 'X':
-        return loses[A]
+from solution_1 import score
 
-def score(A, B):
-    scores = {'rock': 1, 'paper': 2, 'scissors': 3}
-    return [0, 3, 6]['XYZ'.index(B)] + scores[chose(A, B)]
+def score_from_outcome(A, outcome):
+    # 0 for rock, 1 for paper, 2 for scissors
+    choice_a = 'ABC'.index(A)
+    # 0 for draw, 1 for win, 2 for lose
+    result = 'YZX'.index(outcome)
+    b_choice = (choice_a + result) % 3
+    return score(A, 'XYZ'[b_choice])
 
 def solution(path):
     with open(path) as f:
@@ -19,8 +15,9 @@ def solution(path):
     for round in rounds:
         player_A = round[0]
         player_B = round[2]
-        running_score += score(player_A, player_B)
+        running_score += score_from_outcome(player_A, player_B)
     return running_score
 
-print(solution('example_input.txt'))
-print(solution('input.txt'))
+if __name__ == "__main__":
+    print(solution('example_input.txt'))
+    print(solution('input.txt'))
