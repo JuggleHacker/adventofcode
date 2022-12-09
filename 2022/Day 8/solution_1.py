981270b4d4482ed6ee6ca556e7a9d1baeb9e6c67
Cameron Ford <cameron.ford@hotmail.co.uk>
2022-12-09 09:41:32 +0000

    Add day 8 solution

--- 2022/Day 8/solution_1.py
+++ 2022/Day 8/solution_1.py
@@ -1,6 +1,46 @@
+def visible_left_or_right(row_of_trees):
+    total_visible = 2
+    max_left = row_of_trees[0]
+    max_right = max(row_of_trees[2:])
+    for height in row_of_trees[1:-1]:
+        if height > max_left or height > max_right:
+            total_visible += 1
+        max_left = max(max_left, height)
+        max_right = max(max_right, height)
+
 def solution(path):
     with open(path) as f:
-        data = f.read
+        data = f.readlines()
+        rows = len(data)
+        columns = len(data[0][:-1])
+        total_visible = 0
+        max_above_list = [-1] * rows
+        for row_number, row in enumerate(data):
+            max_left = -1
+            trees = row if row[-1] != "\n" else row[:-1]
+            max_below_list = [-1] * rows
+            for trees_to_max in data[row_number + 1:]:
+                for index, height in enumerate(trees_to_max):
+                    if height == '\n':
+                        continue
+                    max_below_list[index] = max(max_below_list[index], int(height))
+            for index, height in enumerate(trees):
+                max_right = -1 if index == columns - 1 else max(int(height) for height in trees[index+1:])
+                if height == "\n":
+                    continue
+                tree_height = int(height)
+                max_above = max_above_list[index]
+                max_below = max_below_list[index]
+                # print(f"Considering {tree_height} at ({row_number},{index})")
+                if tree_height > max_left or tree_height > max_right or tree_height > max_below or tree_height > max_above:
+                    # print(f"{tree_height} in column {index} is visible. Left {max_left} right {max_right} above {max_above} below {max_below}")
+                    total_visible += 1
+                    max_above_list[index] = max(max_above_list[index], tree_height)
+                    max_left = max(max_left, tree_height)
+                # else:
+                #     print(f"{tree_height} in column {index} is NOT visible. Left {max_left} right {max_right} above {max_above} below {max_below}")
 
-print(solution('example_input.txt'))
-# print(solution('input.txt'))
+        print(f"Visible: {total_visible}")
+
+solution('example_input.txt')
+print(solution('input.txt'))
--- 2022/Day 8/solution_2.py
+++ 2022/Day 8/solution_2.py
@@ -1,6 +1,58 @@
+def scenic_score(height, row, col, grid):
+    # print(f"At {row}, {col} height {height}")
+    left = 0
+    right = 0
+    up = 0
+    down = 0
+    left_index = col - 1
+    while left_index >= 0 and grid[row][left_index] < height:
+        left += 1
+        left_index -= 1
+    if left_index != -1:
+        left += 1
+    # print(f"Left: {left}")
+    if left == 0:
+        return 0
+    right_index = col + 1
+    while right_index < len(grid[0]) and grid[row][right_index] < height:
+        right += 1
+        right_index += 1
+    if right_index != len(grid[0]):
+        right += 1
+    # print(f"Right: {right}")
+    if right == 0:
+        return 0
+    up_index = row - 1
+    while up_index >= 0 and grid[up_index][col] < height:
+        up += 1
+        up_index -= 1
+    if up_index != -1:
+        up += 1
+    # print(f"Up: {up}")
+    if up == 0:
+        return 0
+    down_index = row + 1
+    while down_index < len(grid) and grid[down_index][col] < height:
+        down += 1
+        down_index += 1
+    if down_index != len(grid):
+        down += 1
+    # print(f"Down: {down}")
+    return left * right * up * down
+
 def solution(path):
     with open(path) as f:
-        data = f.read
+        data = f.readlines()
+        grid = []
+        for row in data:
+            tree_heights = row[:-1] if row[-1] == "\n" else row
+            grid.append([int(height) for height in tree_heights])
+        max_scenic_score = 0
+        for row_number, row in enumerate(grid):
+            for col_number, tree_height in enumerate(row):
+                this_scenic_score = scenic_score(tree_height, row_number, col_number, grid)
+                max_scenic_score = max(max_scenic_score, this_scenic_score)
+        print(max_scenic_score)
 
-print(solution('example_input.txt'))
-# print(solution('input.txt'))
+# print(solution('example_input.txt'))
+print(solution('input.txt'))
