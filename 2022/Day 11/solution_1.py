9661b355fffa3fa0cb9941da9eb957cb93e6b086
Cameron Ford <cameron.ford@hotmail.co.uk>
2022-12-11 14:20:54 +0000

    Add day 11 solutions

---
+++ 2022/Day 11/solution_1.py
@@ -0,0 +1,56 @@
+class Monkey:
+    def __init__(self, items, operation, divisible_by, true_monkey, false_monkey, inspections=0):
+        self.inspections = inspections
+        self.items = items
+        self.operation = operation
+        self.divisible_by = divisible_by
+        self.true_monkey = true_monkey
+        self.false_monkey = false_monkey
+
+    def __str__(self):
+        return f"Monkey with items {self.items}, operation {self.operation}, test: {self.divisible_by}, true_monkey {self.true_monkey} and false_monkey {self.false_monkey} and inspections {self.inspections}"
+
+def parse_monkey(list_of_attributes):
+    items = list_of_attributes[0][18:]
+    items = [int(x) for x in items.split(', ')]
+    operation = list_of_attributes[1][19:-1]
+    test = int(list_of_attributes[2][21:-1])
+    true_monkey = int(list_of_attributes[3][29])
+    false_monkey = int(list_of_attributes[4][30:])
+    return Monkey(items, operation, test, true_monkey, false_monkey)
+
+
+
+def next_round(list_of_monkeys):
+    for monkey in list_of_monkeys:
+        for worry_level in monkey.items:
+            new_worry_level = eval(monkey.operation.replace('old', str(worry_level)))
+            new_worry_level //= 3
+            monkey.inspections += 1
+            if new_worry_level % monkey.divisible_by == 0:
+                list_of_monkeys[monkey.true_monkey].items.append(new_worry_level)
+            else:
+                list_of_monkeys[monkey.false_monkey].items.append(new_worry_level)
+        monkey.items = []
+
+def solution(path):
+    with open(path) as f:
+        data = f.readlines()
+        monkeys = []
+        i = 1
+        while i < len(data):
+            monkey_to_parse = data[i:i+5]
+            monkeys.append(parse_monkey(monkey_to_parse))
+            i += 7
+        for monkey in monkeys:
+            print(monkey)
+        print()
+        for _ in range(20):
+            next_round(monkeys)
+        inspections = sorted([monkey.inspections for monkey in monkeys])
+        print(inspections[-1] * inspections[-2])
+
+
+
+# print(solution('example_input.txt'))
+print(solution('input.txt'))
